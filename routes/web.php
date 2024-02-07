3bf8ea0e5caada90cbf135cbeba77f8f818e2d74
andresmedina01 <157083769+andresmedina01@users.noreply.github.com>
2024-02-07 12:39:39 -0700

    Update web.php
    se agragaron las rutsa a las vistas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use Illuminate\Support\Facades\Route; 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/principal', 'principal')->name('principal');
+Route::view('/licencias', 'licencias')->name('licencias');
+Route::view('/documentos', 'documentos')->name('documentos');
+Route::view('/ayuda', 'ayuda')->name('ayuda');
+Route::view('/status', 'status')->name('status');
+
+Route::view('/emitir-licencia-en-vivo', 'licencias.emitir-licencia-en-vivo')->name('emitir-licencia-en-vivo');
+Route::view('/emitir-licencia-en-muerto', 'licencias.emitir-licencia-en-muerto')->name('emitir-licencia-en-muerto');
+Route::view('/emitir-licencia-especial', 'licencias.emitir-licencia-especial')->name('emitir-licencia-especial');
+
+Route::view('/ambiental', 'departamentos.ambiental')->name('ambiental');
+Route::view('/civil', 'departamentos.civil')->name('civil');
+Route::view('/electrico', 'departamentos.electrico')->name('electrico');
+Route::view('/instrum y control', 'departamentos.instrum y control')->name('instrum y control');
+Route::view('/mecanico', 'departamentos.mecanico')->name('mecanico');
+Route::view('/operacion', 'departamentos.operacion')->name('operacion');
+Route::view('/quimico', 'departamentos.quimico')->name('quimico');
+Route::view('/seguridad', 'departamentos.seguridad')->name('seguridad');
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+

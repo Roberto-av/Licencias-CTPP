05d4843fe2c20dff8972425b768169fc8bd219bb
Roberto Aviles <avilesrobertoe@gmail.com>
2024-02-16 18:40:12 -0700

    FIxed Register

--- app/Http/Controllers/auth/RegisterController.php
+++ app/Http/Controllers/auth/RegisterController.php
@@ -6,23 +6,20 @@ use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function show(){
-        echo "Estoy aqui";
         return view('auth.register');
     }
 
-    public function register(RegisterRequest $request){
-        echo "Estoy aqui 22222";
-        dd($request->all());
+    public function register(Request $request){
          // Intenta crear un nuevo usuario
          try {
             $user = User::create($request->all());
-
-            return redirect('/login')->with('success', '¡Usuario registrado exitosamente!');
+            
+            return redirect('login')->with('success', '¡Usuario registrado exitosamente!');
         } catch (\Exception $e) {
 
             return redirect()->back()->with('error', 'Error al registrar el usuario: ' . $e->getMessage());

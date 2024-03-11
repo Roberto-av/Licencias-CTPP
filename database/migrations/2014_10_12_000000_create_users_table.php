eca6816939bb7db9833335acdc7f7c350f807f92
Roberto Aviles <avilesrobertoe@gmail.com>
2024-03-11 08:25:31 -0700

    Update migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('rpe')->unique();
+            $table->string('rpe', 5)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
